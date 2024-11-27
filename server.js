c6b6b5c0e8f2435ca2590d948116624517924ad9
frvm <frvm@itu.dk>
2024-11-27 09:56:12 +0100

    execution time server start

--- server.js
+++ server.js
@@ -9,21 +9,16 @@ const PORT = 3000;
 app.use(bodyParser.json());
 
 // Serve static files from the "public" directory
+const start = Date.now();
+
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Log endpoint
-app.post('/log', (req, res) => {
-  const logMessage = req.body; // Assuming logs are sent as JSON
-
-  // Process the log message (e.g., save it, forward it, etc.)
-  console.log('Received log:', logMessage);
-
-  // Respond to the client
-  res.status(200).send('Log received');
-});
 
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+const end = Date.now();
+
+console.log(`Execution time: ${end - start} ms`);
 

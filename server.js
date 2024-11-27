71762b49f66e832fc4aa6ca637696ae2887e9808
frvm <frvm@itu.dk>
2024-11-27 10:28:48 +0100

    Time measurement python

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-// server.js
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const winston = require('winston');
+
 const app = express();
 const PORT = 3000;
 
+// Logger setup
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
@@ -17,33 +18,30 @@ const logger = winston.createLogger({
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
 
+// Timing start
+const start = Date.now();
+
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Log endpoint
-app.post('/log/collision', (req, res) => {
-  const logMessage = req.body; // Assuming logs are sent as JSON
-
-  // Use Bunyan to log the received message
-  logger.info(logMessage, 'collision detected');
-
-  // Respond to the client
-  res.status(200).send('Log received');
-});
-
-// Log endpoint
-app.post('/log/init', (req, res) => {
-  const logMessage = req.body; // Assuming logs are sent as JSON
-
-  // Use Bunyan to log the received message
-  logger.info(logMessage, 'initialized');
-
-  // Respond to the client
-  res.status(200).send('Log received');
-});
-
 // Start the server
 app.listen(PORT, () => {
+  const end = Date.now();
+  const executionTime = end - start;
   console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Execution time: ${executionTime} ms`);
 });
 
+// Collision log endpoint
+app.post('/log/collision', (req, res) => {
+  const logMessage = req.body; // Assuming logs are sent as JSON
+  logger.info(logMessage, 'Collision detected');
+  res.status(200).send('Log received');
+});
+
+// Initialization log endpoint
+app.post('/log/init', (req, res) => {
+  const logMessage = req.body; // Assuming logs are sent as JSON
+  logger.info(logMessage, 'Initialized');
+  res.status(200).send('Log received');
+});

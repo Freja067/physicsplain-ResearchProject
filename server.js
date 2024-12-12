8f5f50d62e7b3a3041dfcdb5c8170e19975f9987
frvm <frvm@itu.dk>
2024-12-12 13:20:48 +0100

    Memory

--- server.js
+++ server.js
@@ -13,12 +13,25 @@ const start = Date.now();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+function logMemoryUsage() {
+  const memoryUsage = process.memoryUsage();
+  console.log('Memory Usage:', {
+    rss: `${(memoryUsage.rss / 1024 / 1024).toFixed(2)} MB`, // Resident Set Size
+    heapTotal: `${(memoryUsage.heapTotal / 1024 / 1024).toFixed(2)} MB`, // Total heap allocated
+    heapUsed: `${(memoryUsage.heapUsed / 1024 / 1024).toFixed(2)} MB`, // Heap actually used
+    external: `${(memoryUsage.external / 1024 / 1024).toFixed(2)} MB` // External memory used
+  });
+}
+
+setInterval(logMemoryUsage, 5000);
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
 
 const end = Date.now();
+
 
 console.log(`Execution time: ${end - start} ms`);
 
